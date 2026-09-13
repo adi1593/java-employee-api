1694ca5d2396f94d95a5c954292bdca5a3806216
adi1593 <117904546+adi1593@users.noreply.github.com>
2026-09-13 16:45:12 +0530

    Delete EmployeeRepository.java

--- EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.employeeapi.repository;
-
-import com.example.employeeapi.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    List<Employee> findByDepartmentIgnoreCase(String department);
-}
